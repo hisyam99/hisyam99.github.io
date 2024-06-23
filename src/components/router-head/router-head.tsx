4d9f06d03776c52ea1dcb96f3af1bd61ffb667da
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-06-23 22:27:01 +0700

    Add PWA Support.

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,5 +1,5 @@
 import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
-
+import * as pwaHead from "@qwikdev/pwa/head";
 import { component$ } from "@builder.io/qwik";
 
 /**
@@ -15,7 +15,14 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+      {pwaHead.meta.map((l) => (
+        <meta key={l.key} {...l} />
+      ))}
+
+      {pwaHead.links.map((l) => (
+        <link key={l.key} {...l} />
+      ))}
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
